43be1d485018d9420e52d0b67bb8fbca55b056ba
K. MagCalen <40416386+kmagcalen@users.noreply.github.com>
2018-09-18 19:04:45 -0400

    Add files
    Added a short program that prints the sum of digits 1-10.

---
+++ intro/printProduct.py
@@ -0,0 +1,14 @@
+'''
+Baby Snakes! Learn Python through worked examples
+Python 3.4.4
+Create a program that prints the product of the first ten positive integers.
+'''
+
+#Start here. Set a variable X equal to the product of the numbers 1 through 10.
+
+X = 1 * 2 * 3 * 4 * 5 * 6 * 7 * 8 * 9 * 10
+
+'''Print X. Note, this only prints the sum of the digits and does not print the
+equation'''
+print(X)
+
